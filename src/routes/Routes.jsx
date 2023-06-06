97a03236741354935391e0e505620152415d7af0
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-07 01:26:08 +0600

    added home route and page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Register/Register";
+import Home from "../pages/Home/Home/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Main></Main>,
         children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
             {
                 path: '/login',
                 element: <Login></Login>
