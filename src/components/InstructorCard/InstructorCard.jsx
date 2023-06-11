a032801437b37a53b9b077528949ec1321f4030c
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-11 23:56:52 +0600

    added dark mode toggle

--- src/components/InstructorCard/InstructorCard.jsx
+++ src/components/InstructorCard/InstructorCard.jsx
@@ -5,23 +5,7 @@ const InstructorCard = ({ singleInstructor }) => {
 
     const { image, name, students } = singleInstructor;
 
-    console.log(singleInstructor);
-
     return (
-        // <div className="card w-full mb-5 md:mb-0 md:w-96 bg-base-100 shadow-md">
-        //     <figure><img src={image} alt="Shoes" /></figure>
-        //     <div className="card-body">
-        //         <h2 className="card-title">
-        //             {name}
-        //             <div className="badge badge-secondary">NEW</div>
-        //         </h2>
-        //         <p>Students: {students}</p>
-        //         <div className="card-actions justify-end">
-        //             <div className="badge badge-outline">Fashion</div>
-        //             <div className="badge badge-outline">Products</div>
-        //         </div>
-        //     </div>
-        // </div>
 
         <div className="card relative w-full mb-5 md:mb-0 md:w-96 bg-base-100 border shadow-sm">
             <figure className="px-10 pt-10">
