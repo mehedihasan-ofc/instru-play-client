d42ecfe22619d1e3a3ef02b0e287586817e580e1
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-12 00:45:29 +0600

    React Awesome Reveal added

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Fade, Zoom } from "react-awesome-reveal";
 
 const SectionTitle = ({ heading, subHeading, description }) => {
     return (
         <div className='text-center'>
-            <p className='text-[#C25934] font-bold text-2xl'>{subHeading}</p>
-            <h3 className='text-[#0C4B65] font-extrabold text-4xl mt-2'>{heading}</h3>
+            <Fade delay={1e3} cascade damping={1e-1}>
+                <p className='text-[#C25934] font-bold text-2xl'>{subHeading}</p>
+            </Fade>
+            <Zoom>
+                <h3 className='text-[#0C4B65] font-extrabold text-4xl mt-2'>{heading}</h3>
+            </Zoom>
         </div>
     );
 };
