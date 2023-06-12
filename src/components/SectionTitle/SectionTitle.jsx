333f48845a657b4394fd492f7e764f62aa0685eb
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-12 23:56:00 +0600

    up to date

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -5,10 +5,10 @@ const SectionTitle = ({ heading, subHeading, description }) => {
     return (
         <div className='text-center'>
             <Fade delay={1e3} cascade damping={1e-1}>
-                <p className='text-[#F24080] font-bold text-2xl'>{subHeading}</p>
+                <p className='text-[#F24080] font-bold text-xl md:text-2xl'>{subHeading}</p>
             </Fade>
             <Zoom>
-                <h3 className='text-[#0C4B65] font-extrabold text-4xl mt-2'>{heading}</h3>
+                <h3 className='text-[#0C4B65] font-extrabold text-2xl md:text-4xl mt-2'>{heading}</h3>
             </Zoom>
         </div>
     );
