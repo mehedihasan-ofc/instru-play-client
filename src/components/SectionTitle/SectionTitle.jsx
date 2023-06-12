0d69c3c47f76084d55e7d947cdbe1fd124856db8
mehedihasan-ofc <mehedi.ofcc@gmail.com>
2023-06-12 23:08:55 +0600

    color change title

--- src/components/SectionTitle/SectionTitle.jsx
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -5,7 +5,7 @@ const SectionTitle = ({ heading, subHeading, description }) => {
     return (
         <div className='text-center'>
             <Fade delay={1e3} cascade damping={1e-1}>
-                <p className='text-[#C25934] font-bold text-2xl'>{subHeading}</p>
+                <p className='text-[#F24080] font-bold text-2xl'>{subHeading}</p>
             </Fade>
             <Zoom>
                 <h3 className='text-[#0C4B65] font-extrabold text-4xl mt-2'>{heading}</h3>
